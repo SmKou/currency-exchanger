51fa620fd2aefc644d704bd719709e442dc41a81
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-30 09:30:59 -0700

    INITFAIL Exchanger getData

--- __tests__/Exchanger.test.js
+++ __tests__/Exchanger.test.js
@@ -2,7 +2,7 @@ import { getExchange, makeApiCall, getData, getError } from '../src/js/bs/Exchan
 
 /* Refer testing_guide.md */
 
-describe("getError", () => {
+describe("Exchanger", () => {
     test("should return error message", () => {
         const target = 'GP';
         const response = {
@@ -10,5 +10,14 @@ describe("getError", () => {
             statusText: 'Not Found'
         }
         expect(getError(target, response)).toBe('An error occurred for GP: 404 Not Found');
+    });
+
+    test("should return conversion rate and result", () => {
+        const target = 'GBP';
+        const response = {
+            "conversion_rate": 0.8623,
+            "conversion_result": 44.7396
+        };
+        expect(getData(target, response)).toBe('The exchange rate for USD to GBP is 1 : 0.86. 52 USD is 44.74 GBP.')
     })
 });
--- src/js/bs/Exchanger.js
+++ src/js/bs/Exchanger.js
@@ -2,7 +2,7 @@ export function getExchange(target, amount = 1) { }
 
 export async function makeApiCall(target, amount) {}
 
-export function getData(response) { }
+export function getData(target, response) { }
 
 export function getError(target, response) { 
     return `An error occurred for ${target}: ${response.status} ${response.statusText}`
