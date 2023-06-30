f25b61ee57d18acd435a00dfab4809f8f0a82c4e
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-30 11:25:29 -0700

    Add UI and test api call

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import { getExchange } from './js/bs/Exchanger';
+import Exchanger from './js/bs/Exchanger';
 
 import './css/fonts.css';
 import './css/styles.css';
 
-// Refer variables from .env => ${process.env.varName}
+const input = {
+    amount: () => {
+        const amount = document.querySelector('#amount').value;
+        return (!amount) ? 1 : amount;
+    },
+    target: () => {
+        return document.querySelector('#currency').value;
+    }
+};
 
 const submit = document.querySelector('#submit-btn');
 submit.addEventListener('click', async e => {
     e.preventDefault();
     const output = document.querySelector('.output');
-    const target = 'GBP';
-    const amount = 52;
-    const result = await getExchange(target, amount);
+    const { target, amount } = input;
+    const result = await Exchanger.getExchange(target(), amount());
     output.innerHTML = result;
 });
--- src/js/bs/Exchanger.js
+++ src/js/bs/Exchanger.js
@@ -1,18 +1,18 @@
-export class Exchanger {
+export default class Exchanger {
     static async getExchange(target, amount = 1) {
         const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${target}/${amount}`;
         const response = await fetch(url);
         console.log(response);
         if (!response.ok)
-            return getError(target, response);
+            return this.getError(target, response);
         else {
             const result = await response.json();
-            return getData(target, amount, result);
+            return this.getData(target, amount, result);
         }
     }
 
     static getData(target, amount, response) {
-        return `The exchange rate is 1 USD to ${response["conversion_rate"].toFixed(2)} ${target}. ${amount} USD is ${response["conversion_result"].toFixed(2)} ${target}.`;
+        return `The exchange rate is 1 USD to ${response["conversion_rate"].toFixed(2)} ${target}\n${amount} USD is ${response["conversion_result"].toFixed(2)} ${target}`;
     }
 
     static getError(target, response) {
