b3a10071225a9479861d1a8687a30c2f623d809d
SmKou <66178856+SmKou@users.noreply.github.com>
2023-07-09 19:51:44 -0700

    Add Form UI

--- src/index.js
+++ src/index.js
@@ -1,42 +1,78 @@
 import Exchanger from './js/bs/Exchanger';
-import { setDefault, setOptions, get } from './js/ui/Form';
+import { setDefault, setOptions } from './js/ui/Form';
 
 import './css/fonts.css';
 import './css/styles.css';
 
 const input = {
-    amount: () => {
-        const input = document.querySelector('#amount');
-        return get(input, 1);
-    },
-    base: () => {
-        const input = document.querySelector('#convert-fr');
-        return get(input, 'USD');
-    },
-    target: () => {
-        const input = document.querySelector('#convert-into');
-        return get(input, 'EUR');
-    },
-    first_entry: false
+    amount: 0,
+    base: '',
+    target: '',
+    base_selected: document.querySelector('.convert_fr .selected'),
+    base_options: document.querySelector('.convert_fr .dropdown_content'),
+    target_selected: document.querySelector('.convert_to .selected'),
+    target_options: document.querySelector('.convert_to .dropdown_content')
 };
 
-const base_currency = document.querySelector('#convert-fr');
-const target_currency = document.querySelector('#convert-into');
-
-setDefault(base_currency, 'base');
-setDefault(target_currency, 'target');
+setDefault(input.base_selected, 'base');
+setDefault(input.target_selected, 'target');
 
 (async () => {
     const result = await Exchanger.getCurrency();
-    setOptions(base_currency, result);
-    setOptions(target_currency, result);
+    setOptions(input.base_options, result);
+    setOptions(input.target_options, result);
 })();
+
+const amount = document.querySelector("[type=number]");
+amount.addEventListener('change', () => { input.amount = amount.value })
+
+const base_btn = document.querySelector('.convert_fr .drop_btn');
+base_btn.addEventListener('click', e => {
+    e.preventDefault();
+    input.base_options.classList.toggle('show');
+});
+
+const target_btn = document.querySelector('.convert_to .drop_btn');
+target_btn.addEventListener('click', e => {
+    e.preventDefault();
+    input.target_options.classList.toggle('show');
+});
+
+input.base_options.addEventListener('click', e => {
+    const id = e.target.id.split('-');
+    input.base = id[0];
+    input.base_selected.innerHTML = `${id[0]}: ${id[1]}`;
+    input.base_options.classList.toggle('show');
+});
+
+input.target_options.addEventListener('click', e => {
+    const id = e.target.id.split('-');
+    input.target = id[0];
+    input.target_selected.innerHTML = `${id[0]}: ${id[1]}`;
+    input.target_options.classList.toggle('show');
+});
 
 const submit = document.querySelector('[type=submit]');
 submit.addEventListener('click', async e => {
     e.preventDefault();
     const output = document.querySelector('.output');
+
+    if (!input.amount) {
+        input.amount = 1;
+        document.querySelector('[type=number]').value = input.amount;
+    }
+
+    if (!input.base) {
+        input.base = 'USD';
+        input.base_selected.innerHTML = 'USD: United States Dollar';
+    }
+
+    if (!input.target) {
+        input.target = 'EUR';
+        input.target_selected.innerHTML = 'EUR: Euro';
+    }
+    
     const { amount, base, target } = input;
-    const result = await Exchanger.getExchange(base(), target(), amount());
+    const result = await Exchanger.getExchange(base, target, amount);
     output.innerHTML = result;
 });
--- src/js/ui/Form.js
+++ src/js/ui/Form.js
@@ -1,24 +1,13 @@
 export function setDefault(elem, type) {
-    const option = document.createElement('option');
-    option.value = '';
-    option.append(document.createTextNode(`Select ${type} currency`));
-    elem.append(option);
+    elem.innerHTML = `Select ${type} currency`;
 }
 
 export function setOptions(elem, options) {
     for (const code of options) {
-        const option = document.createElement('option');
-        option.value = code[0];
-        option.append(document.createTextNode(code[1]));
+        const option = document.createElement('div');
+        option.setAttribute('class', 'option');
+        option.id = `${code[0]}-${code[1]}`;
+        option.append(document.createTextNode(`${code[0]}: ${code[1]}`));
         elem.append(option);
     }
-}
-
-export function get(input, value) {
-    const val = input.value;
-    if (!val) {
-        input.value = value;
-        return value;
-    }
-    return val;
 }
